d4c49bbf69e13daecb9c786d181f2e15a3725ffb
khambley <superdesigngirl@mac.com>
2017-03-29 21:04:18 -0500

    added css and js links and normalize.css

--- functions.php
+++ functions.php
@@ -1,12 +1,17 @@
-function enqueue_my_scripts() {
-    // jQuery is stated as a dependancy of bootstrap-js - it will be loaded by WordPress before the BS scripts 
-    wp_enqueue_script( 'bootstrap-js', '//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js', array('jquery'), true); // all the bootstrap javascript goodness
-}
-add_action('wp_enqueue_scripts', 'enqueue_my_scripts');
-function enqueue_my_styles() {
+<?php
+function mytheme_theme_styles() {
     wp_enqueue_style( 'bootstrap', '//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css' );
-
-    // this will add the stylesheet from it's default theme location if your theme doesn't already
-    //wp_enqueue_style( 'my-style', get_template_directory_uri() . 'css/style.css');
+    wp_enqueue_style( 'normalize', get_template_directory_uri() . 'css/normalize.css');
+    wp_enqueue_style( 'my-style', get_template_directory_uri() . 'css/style.css');
 }
-add_action('wp_enqueue_scripts', 'enqueue_my_styles');
+add_action('wp_enqueue_scripts', 'mytheme_theme_styles');
+
+function mytheme_theme_js() {
+    // jQuery is stated as a dependancy of bootstrap-js - it will be loaded by WordPress before the BS scripts
+    wp_enqueue_script( 'bootstrap-js', '//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js', array('jquery'), true);
+    wp_enqueue_script( 'modernizr-js', get_template_directory_uri() . 'js/modernizr-custom.js', '', '', false);
+}
+add_action('wp_enqueue_scripts', 'mytheme_theme_js');
+
+
+?>
